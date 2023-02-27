322e03ecd3efa85fd15083d1f80a2d1bf91ad1b7
tpapac <tomislav.papac@sum.ba>
2023-02-27 17:18:19 +0100

    sdf

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -335,7 +335,7 @@ class mobile
 
         if ($facetoface->signuptype == MOD_FACETOFACE_SIGNUP_MULTIPLE || (!$signedup && !$managerrequired && $hascap)) {
             // Signup form.
-         //   $mform->display();
+            $mform->display();
         }
 
         $signupispis .= $OUTPUT->box_end();
@@ -345,7 +345,7 @@ class mobile
             'templates' => [
                 [
                     'id' => 'main',
-                    'html' =>'<h1>Test</h1>' . $signupispis,
+                    'html' =>'<h1>Tet</h1>' . $signupispis,
                 ],
             ],
         ];
