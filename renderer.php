cc08367ea97ee75bdf4b5e9d65eac2f863ded9db
tpapac <tomislav.papac@sum.ba>
2023-02-28 22:52:04 +0100

    asd

--- renderer.php
+++ renderer.php
@@ -196,10 +196,10 @@ class mod_facetoface_renderer extends plugin_renderer_base {
 
             // Add row to table.
             $table->data[] = $row;
-            var_dump($table->data);
-            die();
-        }
 
+        }
+        var_dump($table->data);
+        die();
         $output .= html_writer::table($table);
 
         return $output;
