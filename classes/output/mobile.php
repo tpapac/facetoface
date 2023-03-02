7433a6431596beb2877aebb1bd6dd6fa5775ebb4
tpapac <tomislav.papac@sum.ba>
2023-03-02 21:05:44 +0100

    tt

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -269,7 +269,7 @@ class mobile
                     'html' => $OUTPUT->render_from_template('mod_facetoface/form_view', $data),
                 ],
             ],
-            'otherdata' => ['myVar' => $arr],
+            'otherdata' => ['myVar' => '$arr'],
         ];
     }
 
