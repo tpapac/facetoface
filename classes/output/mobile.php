fbfad7b0e62ace1b5a39a6ba0ee89f4c4d3401e0
tpapac <tomislav.papac@sum.ba>
2023-03-16 10:31:05 +0100

    123

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -314,8 +314,8 @@ class mobile
         global $OUTPUT, $DB;
         $args = (object)$args;
         $dir = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
-        require_once($dir . '/config.php');
-        require_once($dir . '/mod/facetoface/lib.php');
+       // require_once($dir . '/config.php');
+        //require_once($dir . '/mod/facetoface/lib.php');
 
 //        $session = \facetoface_get_session($args->s);
 //        $facetoface = $DB->get_record('facetoface', array('id' => $session->facetoface));
