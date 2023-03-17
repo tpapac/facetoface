bbfa59d69d3ca7206dc43378422ffd5902d7cbe7
tpapac <tomislav.papac@sum.ba>
2023-03-17 13:23:49 +0100

    123

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -469,8 +469,40 @@ class mobile
 
     public static function signupConfirm($args)
     {
-        global $OUTPUT;
+        global $DB, $OUTPUT, $CFG, $PAGE, $USER;
         $args = (object)$args;
+        $dir = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
+        require_once($dir . '/config.php');
+        require_once($dir . '/mod/facetoface/lib.php');
+        $session = \facetoface_get_session($args->s);
+        $facetoface = $DB->get_record('facetoface', array('id' => $session->facetoface));
+        $course = $DB->get_record('course', array('id' => $facetoface->course));
+        $cm = get_coursemodule_from_instance("facetoface", $facetoface->id, $course->id);
+        $context = \context_course::instance($course->id);
+
+            // Get signup type.
+            if (!$session->datetimeknown) {
+                $statuscode = MDL_F2F_STATUS_WAITLISTED;
+            } else if (facetoface_get_num_attendees($session->id) < $session->capacity) {
+
+                // Save available.
+                $statuscode = MDL_F2F_STATUS_BOOKED;
+            } else {
+                $statuscode = MDL_F2F_STATUS_WAITLISTED;
+            }
+
+            $submissionid = facetoface_user_signup($session, $facetoface, $course, '', $args->notification, $statuscode) {
+
+                $message = get_string('bookingcompleted', 'facetoface');
+                if ($session->datetimeknown && $facetoface->confirmationinstrmngr) {
+                    $message .= html_writer::empty_tag('br') . html_writer::empty_tag('br')
+                        . get_string('confirmationsentmgr', 'facetoface');
+                } else {
+                    $message .= html_writer::empty_tag('br') . html_writer::empty_tag('br') . get_string('confirmationsent', 'facetoface');
+                }
+
+                $timemessage = 4;
+
         $data = [
             'manager' => $args->manager,
             'notification' => $args->notification,
