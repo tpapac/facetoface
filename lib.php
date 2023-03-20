f5ce9ac306800b4b998eba2bedd130da8c71ab24
tpapac <tomislav.papac@sum.ba>
2023-03-20 11:49:54 +0100

    123

--- lib.php
+++ lib.php
@@ -1898,8 +1898,6 @@ function facetoface_user_signup($session, $facetoface, $course, $discountcode,
     if (!empty($usersignup->id)) {
         $success = $DB->update_record('facetoface_signups', $usersignup);
     } else {
-        var_dump($usersignup);
-        die();
         $usersignup->id = $DB->insert_record('facetoface_signups', $usersignup);
         $success = (bool)$usersignup->id;
     }
