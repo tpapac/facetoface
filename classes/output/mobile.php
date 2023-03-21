4b956c11364bb87d42663c766747629a11a10d83
tpapac <tomislav.papac@sum.ba>
2023-03-21 13:52:21 +0100

    123

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -12,13 +12,13 @@ class mobile
 
     public static function view_facetoface($args)
     {
-        global $OUTPUT, $CFG, $PAGE, $USER;
+        global $DB, $OUTPUT, $CFG, $PAGE, $USER;
         $args = (object)$args;
         $dir = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
         require_once($dir . '/config.php');
         require_once($dir . '/mod/facetoface/lib.php');
         require_once($dir . '/mod/facetoface/renderermobile.php');
-        $DB = \moodle_database::get_instance();
+
         $cmid = \get_coursemodule_from_id('facetoface', $args->cmid);
         $cm = $DB->get_record('course_modules', array('id' => $args->courseid));
         $course = $DB->get_record('course', array('id' => $cm->course));
